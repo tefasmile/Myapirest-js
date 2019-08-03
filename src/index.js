34a1f07220847eba536d234e7fb09dcc7817ab9f
tefasmile <tefaz.n@gmail.com>
2019-08-02 23:38:29 -0500

    hemos anexado nodermon y creado la var de app

--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@
 const express = require('express');
 //se empieza a utilizar el framework y se guarda en app
 const app = express(); 
-//empezamos a utilizar el fw=framework
-app.listen(3000, () => {
-    console.log(`Server on port ${3000}`);
+//requiero de modulo morgan
+const morgan = require('morgan');
+
+//settings
+app.set('port', 3030);//variable del puerto
+
+
+//ejecutando middlewares 
+//un middleware es una funcion que procesa datos antes de que el servidor los reciba
+app.use(morgan('dev'));
+//usar datos que vienen de inputs
+app.use(express.urlencoded({extended: false}));
+//usar y recibir formatos json y entenderlos
+app.use(express.json());
+
+//starting the server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
 });
